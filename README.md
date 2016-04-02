42ae8bac875e15af136d9d3a0d00a71c4400a789
John Hodges <jdhmtl@fastmail.com>
2016-04-02 12:01:24 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bolt Skeleton
+
+Application skeleton for Bolt CMS based apps. Clone, `composer install`, and you're good to go.
---
+++ app/bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+use Bolt\Application;
+use Bolt\Configuration\Composer as Config;
+use Symfony\Component\Yaml\Yaml;
+
+return call_user_func(
+    function() {
+        $root_dir = dirname(__DIR__);
+        require_once $root_dir . '/vendor/autoload.php';
+
+        $config = new Config($root_dir);
+
+        // If we have a .bolt.yml file, update our config object
+        if (file_exists($root_dir . '/.bolt.yml')) {
+            $yaml = Yaml::parse(file_get_contents($root_dir . '/.bolt.yml')) ?: [];
+        }
+
+        if (isset($yaml['paths'])) {
+            foreach ($yaml['paths'] as $key => $path) {
+                $config->setPath($key, $path);
+            }
+        }
+
+        if (isset($yaml['urls'])) {
+            foreach ($yaml['urls'] as $key => $url) {
+                $config->setUrl($key, $url);
+            }
+        }
+
+        $app = new Application(['resources' => $config]);
+
+        $app->initialize();
+
+        return $app;
+    }
+);
